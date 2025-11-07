40729a723cae0edbf823406a159bcf1ba2ca8ff3
MagmaFilda <kamencak.filip@seznam.cz>
2025-11-07 18:10:22 +0100

    Pet Class Update

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -1,4 +1,6 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Pet : MonoBehaviour
 {
@@ -6,6 +8,17 @@ public class Pet : MonoBehaviour
     public string petName { get; private set; }
     public Rarity rarity { get; private set; }
     public int damage { get; private set; }
+
+    public float speed = 0.003f;
+
+    private string mode = "Follow";
+    private Transform breakableTarget;
+
+    private Transform Player;
+    private Transform PetPositions;
+
+    private List<Transform> pets;
+    private Transform[] positions;
 
     private void Awake()
     {
@@ -17,5 +30,63 @@ public class Pet : MonoBehaviour
     private void Start()
     {
         Debug.Log($"{petName} ({rarity}) spawned with damage {damage}");
+
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        PetPositions = Player.Find("EquippedPetSlots");
+
+        PlayerStats.EquippedPets.Add(transform);
+
+        positions = PetPositions.GetComponentsInChildren<Transform>();
+        
+    }
+    private void Update()
+    {
+        if (mode == "Follow")
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            AttackBreakable(transform, breakableTarget);
+        }
+    }
+
+    public void ChangeMode(string change, Transform b)
+    {
+        mode = change;
+        breakableTarget = b;
+    }
+    private void FollowPlayer()
+    {
+        pets = PlayerStats.EquippedPets;
+
+        foreach (Transform pet in pets) // bad kazdy pet kontroluje kazdeho peta, jako funguje, ale chce to pak udelat samostatne
+        {
+            foreach (Transform pos in positions)
+            {
+                if (pos == PetPositions) continue;
+                if (!pos.CompareTag("Equipped"))
+                {
+                    GetToPositon(pet, pos.position);
+                    pos.tag = "Equipped";
+                    break;
+                }
+            }
+        }
+
+        foreach (Transform pos in positions)
+        {
+            pos.tag = "Untagged";
+        }
+    }
+
+    private void AttackBreakable(Transform pet, Transform breakable)
+    {
+        GetToPositon(pet, breakable.position);
+    }
+
+    private void GetToPositon(Transform pet, Vector3 followingPosition)
+    {
+        pet.position = Vector3.MoveTowards(pet.position, followingPosition, speed);
     }
 }
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int coins = 0;
+    public static List<Transform> EquippedPets;
+
+    private PlayerInput playerInput;
+    private InputAction clickAction;
+    private float clicked;
+    private bool canClick = true;
+
+    private Transform targetBreakable;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        clickAction = playerInput.actions.FindAction("Click");
+        EquippedPets = new List<Transform>();
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag("Breakable"))
+            {
+                if (targetBreakable != hit.transform)
+                {
+                    targetBreakable = hit.transform;
+                }           
+            }
+        }
+        else
+        {
+            if (targetBreakable != null)
+            {
+                targetBreakable = null;
+            }
+        }
+
+        if (targetBreakable != null)
+        {
+            clicked = clickAction.ReadValue<float>();
+            if (clicked > 0 && canClick)
+            {
+                foreach (Transform p in EquippedPets)
+                {
+                    Pet pet = p.GetComponent<Pet>();
+
+                    pet.ChangeMode("Attack", targetBreakable);
+                }
+
+                canClick = false;
+                StartCoroutine(ResetClick());
+            }
+        }
+    }
+
+    private IEnumerator ResetClick()
+    {
+        while (clicked > 0)
+        {
+            yield return null;
+        }        
+        canClick = true;
+    }
+}
